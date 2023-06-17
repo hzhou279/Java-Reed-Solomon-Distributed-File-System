429458d9c3e6bab23dec0ffb2df64d107dbd7ce2
Hongru Zhou <hzhou279@wisc.edu>
2023-06-17 16:26:52 -0400

    updated gRPC proto setting

--- src/main/java/edu/cmu/reedsolomonfs/datatype/Metadata.java
+++ src/main/java/edu/cmu/reedsolomonfs/datatype/Metadata.java
@@ -1,5 +1,29 @@
 package edu.cmu.reedsolomonfs.datatype;
+import java.util.List;
 
 public class Metadata {
     
+    private String filePath;
+    private List<Node> nodes;
+
+    public Metadata(String filePath, List<Node> nodes) {
+        this.filePath = filePath;
+        this.nodes = nodes;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List<Node> nodes) {
+        this.nodes = nodes;
+    }
 }
