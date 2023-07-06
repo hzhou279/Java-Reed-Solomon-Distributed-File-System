db2b5a428c53f5ab247ce698eaabc13af8e5579e
TommyChen046 <97989576+TommyChen046@users.noreply.github.com>
2023-07-06 01:39:50 +0000

    change heartbeat checker

--- src/main/java/edu/cmu/reedsolomonfs/server/Master/MasterImpl.java
+++ src/main/java/edu/cmu/reedsolomonfs/server/Master/MasterImpl.java
@@ -11,39 +11,46 @@ import io.grpc.stub.StreamObserver;
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.logging.impl.LogKitLogger;
 
 public class MasterImpl extends edu.cmu.reedsolomonfs.server.MasterServiceGrpc.MasterServiceImplBase{
     
     boolean storageActivated;
-    Map<Integer, Long> lastHeartbeat;
-    static final long TIMEOUT = 6000;
+    Map<Integer, Long> currHeartbeat;
+    Map<Integer, Long> oldHeartbeat;
+    static final long checkInterval = 7000;
 
 
     public MasterImpl(){
         storageActivated = false;
-        lastHeartbeat = new HashMap<Integer, Long>();
+        currHeartbeat = new ConcurrentHashMap<Integer, Long>();
+        oldHeartbeat = new ConcurrentHashMap<Integer, Long>();
+        
         Thread hbc = new heartbeatChecker();
         hbc.start();
     }
 
     // heartbeat routine 
     private class heartbeatChecker extends Thread {
+        
         public void run() {
             while(true) {
                 System.out.println("Checking last heartbeat");
                 if (storageActivated) {
-                    for (Map.Entry<Integer, Long> entry : lastHeartbeat.entrySet()) {
-                        System.out.println("Pass timeout check");
-                        long current = System.currentTimeMillis();
-                        if ((current - entry.getValue()) > TIMEOUT) {
+                    for (Map.Entry<Integer, Long> entry : currHeartbeat.entrySet()) {
+                        if (entry.getValue() == oldHeartbeat.get(entry.getKey())) {
                             // timeout
-                            // TODO: Start recovery;
+                            System.out.println("heartbeat timeout");
                             break;
-                        }
+                        } 
+                        oldHeartbeat.put(entry.getKey(), entry.getValue());
+                        System.out.println("Pass timeout check");
                     }
                 }
                 try {
-                    Thread.sleep(TIMEOUT);
+                    Thread.sleep(checkInterval);
                 } catch (InterruptedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -57,20 +64,27 @@ public class MasterImpl extends edu.cmu.reedsolomonfs.server.MasterServiceGrpc.M
     @Override
     public void heartBeat(HeartbeatRequest request, StreamObserver<HeartbeatResponse> responseObserver) {
         try {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            String serverTag = request.getServerTag();
+
             // storage activated 
             if (!storageActivated) {
                 storageActivated = true;
-            }
+                int chunkservers = 6;
+                for (int i = 0; i < chunkservers; i++) {
+                    oldHeartbeat.put(i, (long)0);
+                }
+            } 
 
 
-            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-            String serverTag = request.getServerTag();
+            
 
             System.out.println("Received Heatbeat in: " + timestamp);
             System.out.println(serverTag);
             
             // update last heartbeat timestamp
-            lastHeartbeat.put(Integer.parseInt(serverTag), System.currentTimeMillis());
+            currHeartbeat.put(Integer.parseInt(serverTag), System.currentTimeMillis());
+
 
 
             HeartbeatResponse response = HeartbeatResponse.newBuilder().setReceive(true).build();
